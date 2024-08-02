89f670f023189d8d82e265804a4b362a3489ebb4
MGiFrAnca <00001114867445sp@al.educacao.sp.gov.br>
2024-08-02 18:37:17 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(1000, 800);
+  background("red");
+}
+
+function draw() {
+  
+  fill("black")
+  stroke("white")
+ if (mouseIsPressed)
+  circle(mouseX,mouseY,55,155);
+  }
